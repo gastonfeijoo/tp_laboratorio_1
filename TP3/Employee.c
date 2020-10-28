7447bea31c5f96a7ff5c65b353df30932aeffe96
gastonfeijoo <tintagel.cornwall@gmail.com>
2020-10-27 22:42:10 -0300

    Parser txt

--- TP3/Employee.c
+++ TP3/Employee.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include "Employee.h"
+#include "utn.h"
 #include <string.h>
 
 Employee* employee_new()
@@ -18,6 +19,115 @@ Employee* employee_newParametros(char* idStr,char* nombreStr,char* horasTrabajad
 
     if (pEmployee!=NULL)
     {
-
+        //retornos hacer
+        employee_setId(pEmployee,atoi(idStr));
+        employee_setNombre(pEmployee,nombreStr);
+        employee_setHorasTrabajadas(pEmployee,atoi(horasTrabajadasStr));
+        employee_setSueldo(pEmployee,atoi(sueldoStr));
     }
+}
+
+int employee_setId(Employee* this,int id)
+{
+    int returnStatus;
+    returnStatus=0;
+
+    if (this!=NULL && id>0)
+    {
+        this->id=id;
+        returnStatus=1;
+    }
+    return returnStatus;
+}
+int employee_getId(Employee* this,int* id)
+{
+    int returnStatus;
+    returnStatus=0;
+
+    if(this!=NULL && id!=NULL)
+    {
+        *id=this->id;
+        returnStatus=1;
+    }
+
+    return returnStatus;
+}
+
+int employee_setNombre(Employee* this,char* nombre)
+{
+    int returnStatus;
+    returnStatus=0;
+
+    if (this!=NULL && esAlfaNumerico(nombre))
+    {
+        strcpy(this->nombre,nombre);
+        returnStatus=1;
+    }
+    return returnStatus;
+}
+
+int employee_getNombre(Employee* this,char* nombre)
+{
+    int returnStatus;
+    returnStatus=0;
+
+    if(this!=NULL && nombre!=NULL)
+    {
+        strcpy(*nombre,this->nombre);
+        returnStatus=1;
+    }
+
+    return returnStatus;
+}
+
+int employee_setHorasTrabajadas(Employee* this,int horasTrabajadas)
+{
+    int returnStatus;
+    returnStatus=0;
+
+    if (this!=NULL && horasTrabajadas>0)
+    {
+        this->horasTrabajadas=horasTrabajadas;
+        returnStatus=1;
+    }
+    return returnStatus;
+}
+int employee_getHorasTrabajadas(Employee* this,int* horasTrabajadas)
+{
+    int returnStatus;
+    returnStatus=0;
+
+    if(this!=NULL && horasTrabajadas!=NULL)
+    {
+        *horasTrabajadas=this->horasTrabajadas;
+        returnStatus=1;
+    }
+
+    return returnStatus;
+}
+
+int employee_setSueldo(Employee* this,int sueldo)
+{
+    int returnStatus;
+    returnStatus=0;
+
+    if (this!=NULL && sueldo>0)
+    {
+        this->sueldo=sueldo;
+        returnStatus=1;
+    }
+    return returnStatus;
+}
+int employee_getSueldo(Employee* this,int* sueldo)
+{
+    int returnStatus;
+    returnStatus=0;
+
+    if(this!=NULL && sueldo!=NULL)
+    {
+        *sueldo=this->sueldo;
+        returnStatus=1;
+    }
+
+    return returnStatus;
 }
--- TP3/parser.c
+++ TP3/parser.c
@@ -5,12 +5,87 @@
 
 int parser_EmployeeFromText(FILE* pFile, LinkedList* pArrayListEmployee)
 {
+    //probar
+
+    char id[100];
+    char nombre[100];
+    char horasTrabajadas[100];
+    char sueldo[100];
+    int rFscanF;
+
+
+    //ya viene abierto
+    //pFile=fopen("data.csv","r");
+
+    if (pFile==NULL)
+    {
+        return -1;
+    }
+    while(!feof(pFile))
+    {
+        rFscanF=fscanf(pFile, "%[^,],%[^,],%[^,],%[^,]\n", id, nombre, horasTrabajadas,sueldo);
+        if (rFscanF==4)
+        {
+            Employee* pAux = employee_new();
+
+            //usar el constructor por parametros
+            employee_setId(pAux,atoi(id));
+            employee_setNombre(pAux,nombre);
+            employee_setHorasTrabajadas(pAux,atoi(horasTrabajadas));
+            employee_setSueldo(pAux,atoi(sueldo));
+            //
+            ll_add(pArrayListEmployee,pAux);
+        }
+        else
+        {
+            break;
+        }
+    }
+
+    fclose(pFile);
+
     return 1;
 
 }
 
 int parser_EmployeeFromBinary(FILE* pFile, LinkedList* pArrayListEmployee)
 {
+
+
+    char id[100];
+    char nombre[100];
+    char horasTrabajadas[100];
+    char sueldo[100];
+    int rFread;
+
+
+
+    pFile=fopen("data.csv","r");
+
+    if (pFile==NULL)
+    {
+        return -1;
+    }
+    while(!feof(pFile))
+    {
+        rFread=fread(pFile, "%[^,],%[^,],%[^,],%[^,]\n", id, nombre, horasTrabajadas,sueldo);
+        if (rFread==4)
+        {
+            Employee* pAux = employee_new();
+            employee_setId(pAux,atoi(id));
+            employee_setNombre(pAux,nombre);
+            employee_setHorasTrabajadas(pAux,atoi(horasTrabajadas));
+            employee_setSueldo(pAux,atoi(sueldo));
+            ll_add(pArrayListEmployee,pAux);
+        }
+        else
+        {
+            break;
+        }
+    }
+
+    fclose(pFile);
+
 
     return 1;
 }
